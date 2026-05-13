61e09bd890a8c6607462dd9bd3d2dbc8600a2e6f
kingz1127 <osunyingboadedeji1@gmail.com>
2026-05-13 19:09:33 +0100

    outreach 2 sub

--- src/main/java/com/example/InnerCityBackend/model/dto/request/CreateSubcategoryRequest.java
+++ src/main/java/com/example/InnerCityBackend/model/dto/request/CreateSubcategoryRequest.java
@@ -1,10 +1,13 @@
 package com.example.InnerCityBackend.model.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CreateSubcategoryRequest {
+
+    @JsonProperty("category_id")
     @NotBlank String categoryId;
     @NotBlank String name;
     String description;
