8076c6641c4d464e13c227f8eb4817cea83462e7
kingz1127 <osunyingboadedeji1@gmail.com>
2026-05-13 21:07:22 +0100

    flutter

--- src/main/java/com/example/InnerCityBackend/security/SecurityConfig.java
+++ src/main/java/com/example/InnerCityBackend/security/SecurityConfig.java
@@ -108,11 +108,14 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:5173",
-                "http://localhost:3000",
-                "https://icm-web-beta.vercel.app"
-        ));
+//        configuration.setAllowedOrigins(Arrays.asList(
+//                "http://localhost:5173",
+//                "http://localhost:3000",
+//                "https://icm-web-beta.vercel.app"
+//        ));
+
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+
         configuration.setAllowedMethods(Arrays.asList(
                 "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"
         ));
